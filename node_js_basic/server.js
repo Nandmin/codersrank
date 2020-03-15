066a5bbc885d16278be932f149f2cb201f0649fa
Németh András <andras78.nemeth@gmail.com>
2020-03-16 00:33:29 +0100

    node js basic

---
+++ node_js_basic/server.js
@@ -0,0 +1,20 @@
+var multiply = require('./multiply-module');
+var fs = require('fs');
+
+var http = require('http');
+
+function onRequest(request, response){
+  response.writeHead(200, {'Content-Type': 'text/html'});
+  fs.readFile('./index.html', null, function(error, data){
+    if (error) {
+      response.writeHead(404);
+      response.write('File not found');
+    } else {
+      response.write(data);
+    }
+    response.end();
+  });
+}	
+
+http.createServer(onRequest).listen(3000);
+console.log('The server is running...');
